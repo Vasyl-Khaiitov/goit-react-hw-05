a673c7b5bd8fe460daae847107be993275c35d71
MazaFaaka <haitovvasyl@gmail.com>
2025-05-13 20:45:10 +0200

    compled starting senttings

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from 'path/to/pages/Home';
+import About from 'path/to/pages/About';
+import Products from 'path/to/pages/Products';
 
 export default function App() {
   return (
-    <>
-   Hello
-    </>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+          <Route path="cast" element={<MovieCast />} />
+          <Route path="reviews" element={<MovieReviews />} />
+        </Route>
+      </Routes>
+    </div>
   );
 }
