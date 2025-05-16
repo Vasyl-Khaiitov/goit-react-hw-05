ebc6ed6d642499c21008e325ce73fd11b916a314
MazaFaaka <haitovvasyl@gmail.com>
2025-05-16 17:52:35 +0200

    compled structur project

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from 'path/to/pages/Home';
-import About from 'path/to/pages/About';
-import Products from 'path/to/pages/Products';
+import Movies from 'path/to/pages/Products';
 
 export default function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/products" element={<Products />} />
+        <Route path="/movies" element={<Movies />} />
         <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
           <Route path="cast" element={<MovieCast />} />
           <Route path="reviews" element={<MovieReviews />} />
         </Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
